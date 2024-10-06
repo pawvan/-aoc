ba8faab510d7371a59e068a441ca18e62741f1c2
redxfy <150579399+redxfy@users.noreply.github.com>
2024-10-06 12:11:49 +0530

    algebraic_data_types_(adts).purs

---
+++ algebraic_data_types_(adts).purs
@@ -0,0 +1,12 @@
+
+
+--Sum Types: Define a type that can be one of several alternatives.
+
+
+data shape= circle | shape 
+
+--Product Types: Define a type that groups multiple values.
+
+
+data Point = Point Int Int 
+
