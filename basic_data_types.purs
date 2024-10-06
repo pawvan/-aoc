47cff8c126ab32dd9b87affd66400aef7722bb6b
redxfy <150579399+redxfy@users.noreply.github.com>
2024-10-06 12:08:54 +0530

    basic_data_types.purs

---
+++ basic_data_types.purs
@@ -0,0 +1,15 @@
+--Int: Represents integer numbers.
+
+x:: Int
+x=21
+
+-- String: Represents text.
+
+name :String 
+name ="hi purescript!";
+
+--Boolean: Represents truth values 
+
+isTrue:Boolean 
+
+isTrue=true 
